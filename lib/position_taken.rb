a3adb694648b224447cdb8747385d39ebbf97a43
Charles Mwirigi <mwirigi.charles@gmail.com>
2018-05-22 19:17:46 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(array, index)
-  if array[index] = " " 
+  if array[index] == " " || array[index] == "" || array[index] == nil
     false
   end
 
